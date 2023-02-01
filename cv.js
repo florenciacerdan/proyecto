b84c461773095073a630dff8d54fb88dcb40166f
FlorenciaCerdan <florencia.cerdan@gmail.com>
2023-02-01 18:02:04 -0300

    primer commit

---
+++ cv.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var x = document.getElementById("tele");
+    if (x.innerHTML === "   ") {
+      x.innerHTML = "+54 261 5936759";
+    } else { 
+      x.innerHTML = "   ";
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("mail");
+    if (x.innerHTML === "   ") {
+      x.innerHTML = "aguilarpilar@gmail.com";
+    } else { 
+      x.innerHTML = "   ";
+    }
+  }
+  function anotherfunction() {
+    var x = document.getElementById("local");
+    if (x.innerHTML === "   ") {
+      x.innerHTML = "Joaquin V. Gonzales, 340, Ciudad, Mendoza";
+    } else { 
+      x.innerHTML = "   ";
+    }
+  }
+  
